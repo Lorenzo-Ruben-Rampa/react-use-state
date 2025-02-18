b72211988605579d89ab3555a0a2fcb497740abd
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-02-18 14:28:06 +0100

    bonus nessun linguaggio selezionato

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,8 +3,7 @@ import CardList from "./products/CardList.jsx";
 
 export default function Main({ languages }) {
 
-
-    const [selectedLanguage, setSelectedLanguage] = useState(languages[0]);
+    const [selectedLanguage, setSelectedLanguage] = useState(null);
 
     const handleLanguageSelect = (language) => {
         setSelectedLanguage(language);
@@ -14,12 +13,16 @@ export default function Main({ languages }) {
         <main>
             < CardList languages={languages} onLanguageSelect={handleLanguageSelect} />
             <div className="container">
-                {selectedLanguage && (
+                {selectedLanguage ? (
                     <div className="accordion">
                         <div className="accordion__title">
                             <h3>{selectedLanguage.title}</h3>
                             <p>{selectedLanguage.description}</p>
                         </div>
+                    </div>
+                ) : (
+                    <div className="accordion">
+                        <p>nessun linguaggio selezionato</p>
                     </div>
                 )}
             </div>
